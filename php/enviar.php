898888c4a11725f12e6d35c46bec97a535374772
Caio Fernando <mr.kaka77@gmail.com>
2018-08-06 12:17:24 -0300

    ajustes

--- php/enviar.php
+++ php/enviar.php
@@ -15,16 +15,16 @@ try {
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'Emailaqui';                 // SMTP username
-    $mail->Password = 'Senhaaqui';                           // SMTP password
+    $mail->Username = 'Emailaqui';                        // SMTP username
+    $mail->Password = 'Senhaaqui';                        // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom("$email", "$nome");
-    $mail->addAddress('wel345h@gmail.com', 'Site');     // Add a recipient
+    $mail->addAddress('excbrso@gmail.com', 'Site');       // Add a recipient
 
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
